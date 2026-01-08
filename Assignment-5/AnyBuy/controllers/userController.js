52097e88d262c27e31a2db5d0a638eaeb2ad0dfb
Piyush <piyushpatil0031@gmail.com>
2026-01-08 15:57:17 +0530

    assignment-5: fashion website  completed

---
+++ Assignment-5/AnyBuy/controllers/userController.js
@@ -0,0 +1,66 @@
+const Product = require( '../models/Product.js');
+const Cart = require( '../models/Cart.js');
+
+// Render Landing Page
+const getLanding = (req, res) => {
+    res.render('landing');
+};
+
+// Render Home (Product Listing)
+const getHome = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.render('home', { products, user: req.session.user });
+    } catch (error) {
+        console.error(error);
+        res.send("Error loading products");
+    }
+};
+
+// Render Cart
+const getCart = async (req, res) => {
+    try {
+        if (!req.session.user) return res.redirect('/auth/login');
+
+        const cart = await Cart.findOne({ userId: req.session.user._id }).populate('items.productId');
+        res.render('cart', { cart: cart || { items: [] }, user: req.session.user });
+    } catch (error) {
+        console.error(error);
+        res.send("Error loading cart");
+    }
+};
+
+// Add Item to Cart
+const addToCart = async (req, res) => {
+    try {
+        if (!req.session.user) return res.redirect('/auth/login');
+
+        const { productId } = req.body;
+        let cart = await Cart.findOne({ userId: req.session.user._id });
+
+        if (!cart) {
+            cart = new Cart({ userId: req.session.user._id, items: [] });
+        }
+
+        const itemIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+
+        if (itemIndex > -1) {
+            cart.items[itemIndex].quantity += 1;
+        } else {
+            cart.items.push({ productId, quantity: 1 });
+        }
+
+        await cart.save();
+        res.redirect('/cart');
+    } catch (error) {
+        console.error(error);
+        res.redirect('/home');
+    }
+};
+
+module.exports = {
+    getLanding,
+    getHome,
+    getCart,
+    addToCart
+};
