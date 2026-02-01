3af51cb5f7e8ffac102ea27a557280dd8082b49f
Piyush <piyushpatil0031@gmail.com>
2026-02-01 12:09:31 +0530

    dockerized tic-tac-toe and published on docker hub

--- Assignment-5/AnyBuy/controllers/userController.js
+++ Assignment-5/AnyBuy/controllers/userController.js
@@ -1,5 +1,6 @@
 const Product = require( '../models/Product.js');
 const Cart = require( '../models/Cart.js');
+const Order = require( '../models/Order.js');
 
 // Render Landing Page
 const getLanding = (req, res) => {
@@ -58,9 +59,32 @@ const addToCart = async (req, res) => {
     }
 };
 
+const placeOrder = async (req,res) => {
+    try {
+        if(!req.session.user) return res.redirect(`/auth/login`);
+
+        const { products, payment} = req.body;
+        const userid = req.session.user._id;
+
+        if(!products || !payment) return res.status(500).json({message: "There is no item in the cart!"});
+
+        const newOrder = new Order({
+            items: populate(products),
+            payment,
+            customerID: userid
+        });
+
+        const ordered = await Order.save(newOrder);
+        if(ordered) return res.status(200).json({message: "Order placed successfully!"});
+    } catch (error) {
+        return res.status(500).json({message: "Internal Server Error!", error});
+    }
+}
+
 module.exports = {
     getLanding,
     getHome,
     getCart,
-    addToCart
+    addToCart,
+    placeOrder
 };
